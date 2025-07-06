75630d877e5669ac73eb37fac158efddab445ef4
arjun r <arjun.rdell@gmail.com>
2025-07-06 23:42:37 +0530

    minor change to reduce server load

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const nodemailer = require("nodemailer");
 const express = require('express');
 const cors = require('cors');
 
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 15 minutes
+  max: 7, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes."
+});
+
 const app = express()
+app.use(limiter);
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
